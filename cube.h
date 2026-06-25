e1f2e135a8581c3dd7401701887e29e76c4f8318
Nicolas Bigot <nicolasbigot@MacBook-Air-de-Nicolas.local>
2026-06-25 19:53:58 +0200

    new

--- cube.h
+++ cube.h
@@ -20,12 +20,35 @@
 #define BLUE 4
 #define GREEN 5
 
+#define UP 0
+#define DOWN 1
+#define FRONT 2
+#define BACK 3
+#define LEFT 4
+#define RIGHT 5
+
 typedef struct	s_cube
 {
 	int face[6][3][3];
 }				t_cube;
 
 t_cube	cube_init(void);
+
 void	ft_display(void);
+
+void	rotate_face_cw(t_list *cube, int face);
+void	rotate_face_ccw(t_list *cube, int face);
+void	ft_move_u(t_cube *cube);
+void	ft_move_u_prime(t_cube *cube);
+void	ft_move_d(t_cube *cube);
+void	ft_move_d_prime(t_cube *cube);
+void	ft_move_f(t_cube *cube);
+void	ft_move_f_prime(t_cube *cube);
+void	ft_move_b(t_cube *cube);
+void	ft_move_b_prime(t_cube *cube);
+void	ft_move_l(t_cube *cube);
+void	ft_move_l_prime5t_cube *cube°;
+void	ft_move_r(t_cube *cube);
+void	ft_move_r_prime(t_cube *cube);
 
 #endif
