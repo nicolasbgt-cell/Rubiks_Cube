5a2478521e6466abea8bbcf032f6a64eb1f20e99
Nicolas Bigot <nicolasbigot@MacBook-Air-de-Nicolas.local>
2026-06-26 14:12:20 +0200

    good

--- display.c
+++ display.c
@@ -12,46 +12,84 @@
 
 #include "cube.h"
 #include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 
-void	ft_print_cell(char *letter, char *color)
+void	ft_print_cell(int color)
 {
-	write(1, color, strlen(color));
-	write(1, letter, 3);
-	write(1, "\033[0m", 4);
+	if (color == WHITE)
+		printf("\033[37m[W]\033[0m");
+	else if (color == YELLOW)
+		printf("\033[33m[Y]\033[0m");
+	else if (color == RED)
+		printf("\033[31m[R]\033[0m");
+	else if (color == ORANGE)
+		printf("\033[38;5;208m[O]\033[0m");
+	else if (color == BLUE)
+		printf("\033[34m[B]\033[0m");
+	else if (color == GREEN)
+		printf("\033[32m[G]\033[0m");
 }
 
 void	ft_display(t_cube *cube)
 {
-	int	row;
-	int	col;
+	int row;
+	int col;
 
 	row = 0;
-	while (row < 9)
+	while (row < 3)
 	{
+		write(1, "         ", 9);
 		col = 0;
-		while (col < 12)
+		while (col < 3)
 		{
-			if ((col < 3 || col > 5) && (row < 3 || row > 5))
-				write(1, "   ", 3);
-			else
-			{
-				if (row < 3)
-					ft_print_cell("[R]", "\033[31m");
-				else if (row > 5)
-					ft_print_cell("[O]", "\033[38;5;208m");
-				else if (col < 3)
-					ft_print_cell("[B]", "\033[34m");
-				else if (col < 6)
-					ft_print_cell("[W]", "\033[37m");
-				else if (col < 9)
-					ft_print_cell("[G]", "\033[32m");
-				else
-					ft_print_cell("[Y]", "\033[33m");
-			}
+			ft_print_cell(cube->face[UP][row][col]);
 			col++;
 		}
-		write(1, "\n", 1);
+		printf("\n");
+		row++;
+	}
+	row = 0;
+	while (row < 3)
+	{
+		col = 0;
+		while (col < 3)
+		{
+			ft_print_cell(cube->face[LEFT][row][col]);
+			col++;
+		}
+		col = 0;
+		while (col < 3)
+		{
+			ft_print_cell(cube->face[FRONT][row][col]);
+			col++;
+		}
+		col = 0;
+		while (col < 3)
+		{
+			ft_print_cell(cube->face[RIGHT][row][col]);
+			col++;
+		}
+		col = 0;
+		while (col < 3)
+		{
+			ft_print_cell(cube->face[BACK][row][col]);
+			col++;
+			}
+		printf("\n");
+		row++;
+	}
+	row = 0;
+	while (row < 3)
+	{
+		write(1, "         ", 9);
+		col = 0;
+		while (col < 3)
+		{
+			ft_print_cell(cube->face[DOWN][row][col]);
+			col++;
+		}
+		printf("\n");
 		row++;
 	}
 }
