390d886a0e4d9fde2bc40daaf61b5ada952f46cc
Nicolas Bigot <nicolasbigot@MacBook-Air-de-Nicolas.local>
2026-06-26 15:45:07 +0200

    shuffle

--- shuffle.c
+++ shuffle.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cube.h"
+#include <stdlib.h>
+#include <time.h>
 
+void	ft_shuffle(t_cube *cube)
+{
+	int	i;
+	int	move;
+
+	srand(time(NULL));
+	i = 0;
+	while (i < 20)
+	{
+		move = rand() % 12;
+		if (move == 0)
+			ft_move_u(cube);
+		else if (move == 1)
+			ft_move_u_prime(cube);
+		else if (move == 2)
+			ft_move_d(cube);
+		else if (move == 3)
+			ft_move_d_prime(cube);
+		else if (move == 4)
+			ft_move_f(cube);
+		else if (move == 5)
+			ft_move_f_prime(cube);
+		else if (move == 6)
+			ft_move_b(cube);
+		else if (move == 7)
+			ft_move_b_prime(cube);
+		else if (move == 8)
+			ft_move_l(cube);
+		else if (move == 9)
+			ft_move_l_prime(cube);
+		else if (move == 10)
+			ft_move_r(cube);
+		else if (move == 11)
+			ft_move_r_prime(cube);
+		i++;
+	}
+}
