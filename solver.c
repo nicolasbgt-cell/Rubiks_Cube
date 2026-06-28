63d980f35e7522cf65e7a236ef06799739bf095f
Nicolas Bigot <nicolasbigot@MacBook-Air-de-Nicolas.local>
2026-06-28 19:45:55 +0200

    is_goal

--- solver.c
+++ solver.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cube.h"
 
+int	heuristic(t_cube *cube)
+int	is_goal(t_cube *cube)
+{
+	int	f;
+	int	i;
+	int	j;
+	int	ref;
+
+	f = 0;
+	i = 0;
+	j = 0;
+	while (f < 6)
+	{
+		ref =cube->face[f][0][0];
+		while (i < 3)
+		{
+			while (j < 3)
+			{
+				if (cube->face[f][i][j] != ref)
+					return (0);
+				j++;
+			}
+			i++;
+		}
+		f++;
+		i = 0;
+		j = 0;
+	}
+	return (1);
+}
+
+t_cube	apply_move(t_cube *cube, int move)
+int	search(t_cube *path[], int g, int limit, int depth)
+void	ida_star(t_cube cube)
