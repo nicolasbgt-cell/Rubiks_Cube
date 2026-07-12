cad5c9f89a66420447db6ec61ecc60e8755166ed
Nicolas Bigot <nicolasbigot@MacBook-Air-de-Nicolas.local>
2026-07-12 17:50:58 +0200

    align down

--- strategy.c
+++ strategy.c
@@ -55,3 +55,35 @@ void	ft_to_down(t_cube *cube)
 		ft_move_l(cube);
 	}
 }
+
+void	ft_align_down(t_cube *cube)
+{
+	if (cube->face[DOWN][0][1] == WHITE)
+	{
+		while (cube->face[FRONT][2][1] != RED)
+			ft_move_d(cube);
+		ft_move_f(cube);
+		ft_move_f(cube);
+	}
+	if (cube->face[DOWN][1][2] == WHITE)
+	{
+		while (cube->face[RIGHT][2][1] != GREEN)
+			ft_move_d(cube);
+		ft_move_r(cube);
+		ft_move_r(cube);
+	}
+	if (cube->face[DOWN][2][1] == WHITE)
+	{
+		while (cube->face[BACK][2][1] != ORANGE)
+			ft_move_d(cube);
+		ft_move_f(cube);
+		ft_move_f(cube);
+	}
+	if (cube->face[DOWN][1][0] == WHITE)
+	{
+		while (cube->face[LEFT][2][1] != BLUE)
+			ft_move_d(cube);
+		ft_move_l(cube);
+		ft_move_l(cube);
+	}
+}
